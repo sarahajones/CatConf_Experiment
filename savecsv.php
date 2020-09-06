f0c1714914d35788a429eb42d0dcc979f8559802
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-09-06 16:19:08 +0100

    more saving
    * Save files on server now use an experimentName_v#.#.#_userID.csv name format
    * Documented savecsv.php

--- savecsv.php
+++ savecsv.php
@@ -1,4 +1,16 @@
 <?php
+/*
+ * Save incoming data from javascript experiments to a .csv file
+ * Expects json input with fields:
+ * user_id {string} alphanumeric user id
+ * [experiment] {string} alphanumeric experiment string (not needed if user_id == "test_id")
+ * [version] {number[]} version numbering (not needed if user_id == "test_id")
+ * csv {string} file contents in .csv format
+ *
+ * Returns a HTTP response with 200 on success and body text saying so.
+ * On error returns HTTP response != 200 and exception details in the body.
+ */
+
 $post_data = json_decode(file_get_contents('php://input'), true);
 try {
     // the directory "data" must be writable by the server
@@ -6,11 +18,20 @@ try {
     if(!preg_match('/[a-zA-Z0-9]+/', $name))
         throw new Exception('Invalid user ID');
     if($name == "test_id") {
+        $name = "data/public/$name.csv";
         $data = "";
     } else {
+        $experiment_name = $post_data['experiment'];
+        // Version should be a numeric array (preferably major, minor, release)
+        $experiment_version = $post_data['version'];
+        if(!preg_match('/[a-zA-Z0-9]+/', $experiment_name))
+            throw new Exception('Invalid experiment name');
+        if (count($experiment_version) != count(array_filter($experiment_version, 'is_numeric'))) {
+            throw new Exception(('Invalid experiment version'));
+        }
+        $name = "data/public/" . $experiment_name . "_v" . join('.', $experiment_version) . "_$name.csv";
         $data = $post_data['csv'];
     }
-    $name = "data/public/$name.csv";
     // write the file to disk
     file_put_contents($name, $data);
     http_response_code(200);
